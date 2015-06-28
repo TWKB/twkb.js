50b82730d948d46cfe6cd89d408cc63bd5397f47
Björn Harrtell <bjorn@wololo.org>
2015-06-28 10:29:27 +0200

    Refactor GeoJSON translation code

--- src/toGeoJSON.js
+++ src/toGeoJSON.js
@@ -12,35 +12,60 @@ module.exports = function(buffer, startOffset, howMany) {
     
   var features = [];
   
+  var typeMap = {};
+  typeMap[constants.POINT] = 'Point';
+  typeMap[constants.LINESTRING] = 'LineString';
+  typeMap[constants.POLYGON] = 'Polygon';
+  
+  var createGeometry = function(type, coordinates) {
+    return {
+      type: typeMap[type],
+      coordinates: coordinates
+    };
+  };
+  
+  var createFeature = function(type, ta_struct, g, i) {
+    return {
+      type: "Feature",
+      id: ta_struct.has_idlist ? ta_struct.res.ids[i] : undefined,
+      geometry: transforms[type]({res: g, ndims: ta_struct.ndims})
+    };
+  };
+  
+  var createMultiTransform = function(type, ta_struct) {
+    return ta_struct.res.geoms.map(function(g, i) {
+      return createFeature(type, ta_struct, g, i);
+    });
+  };
+  
+  var transforms = {};
+  transforms[constants.POINT] = function(ta_struct) {
+    return createGeometry(constants.POINT, toCoords(ta_struct.res, ta_struct.ndims)[0]);
+  };
+  transforms[constants.LINESTRING] = function(ta_struct) {
+    return createGeometry(constants.LINESTRING, toCoords(ta_struct.res, ta_struct.ndims));
+  };
+  transforms[constants.POLYGON] = function(ta_struct) {
+    return createGeometry(constants.POLYGON, ta_struct.res.map(function(c) {
+      return toCoords(c, ta_struct.ndims);
+    }));
+  };
+  transforms[constants.MULTIPOINT] = function(ta_struct) {
+    return createMultiTransform(constants.POINT, ta_struct);
+  };
+  transforms[constants.MULTILINESTRING] = function(ta_struct) {
+    return createMultiTransform(constants.LINESTRING, ta_struct);
+  };
+  transforms[constants.MULTIPOLYGON] = function(ta_struct) {
+    return createMultiTransform(constants.POLYGON, ta_struct);
+  };
+  
   while (ta_struct.cursor < ta_struct.bufferLength) {
     readBuffer(ta_struct);
-    var geometry = {};
-    switch(ta_struct.type) {
-      case constants.POINT:
-        geometry.type = "Point";
-        geometry.coordinates = toCoords(ta_struct.res, ta_struct.ndims)[0];
-        features.push({ type: "Feature", geometry: geometry });
-        break;
-      case constants.LINESTRING:
-        geometry.type = "LineString";
-        geometry.coordinates = toCoords(ta_struct.res, ta_struct.ndims);
-        features.push({ type: "Feature", geometry: geometry });
-        break;
-      case constants.POLYGON:
-        geometry.type = "Polygon";
-        geometry.coordinates = ta_struct.res.map(function(c) {
-          return toCoords(c, ta_struct.ndims);
-        });
-        features.push({ type: "Feature", geometry: geometry });
-        break;
-      case constants.MULTIPOINT:
-        ta_struct.res.geoms.forEach(function(g, i) {
-          var geometry = {};
-          geometry.type = "Point";
-          geometry.coordinates = toCoords(g, ta_struct.ndims)[0];
-          features.push({ type: "Feature", id: ta_struct.res.ids[i], geometry: geometry });
-        });
-        break;
+    if (ta_struct.res.geoms) {
+      features = features.concat(transforms[ta_struct.type](ta_struct));
+    } else {
+      features.push({ type: "Feature", geometry: transforms[ta_struct.type](ta_struct) });
     }
   }
   
