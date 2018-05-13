6ff62d4567f4a2af38c46f0837290b201769c697
Björn Harrtell <bjorn@wololo.org>
2018-05-13 19:20:11 +0200

    Replace deprecated API

--- test/twkb.spec.js
+++ test/twkb.spec.js
@@ -8,7 +8,7 @@ describe('twkb', function () {
   describe('toGeoJSON', function () {
 
     it('should decode point to geojson', function () {
-      var g = twkb.toGeoJSON(new Buffer('01000204', 'hex'))
+      var g = twkb.toGeoJSON(Buffer.from('01000204', 'hex'))
       assert.deepEqual(g, {
          type: 'FeatureCollection',
          features: [
@@ -24,7 +24,7 @@ describe('twkb', function () {
     })
 
     it('should decode linestring to geojson', function () {
-      var g = twkb.toGeoJSON(new Buffer('02000202020808', 'hex'))
+      var g = twkb.toGeoJSON(Buffer.from('02000202020808', 'hex'))
       assert.deepEqual(g, {
          type: 'FeatureCollection',
          features: [
@@ -40,7 +40,7 @@ describe('twkb', function () {
     })
 
     it('should decode polygon to geojson', function () {
-      var g = twkb.toGeoJSON(new Buffer('03031b000400040205000004000004030000030500000002020000010100', 'hex'))
+      var g = twkb.toGeoJSON(Buffer.from('03031b000400040205000004000004030000030500000002020000010100', 'hex'))
       assert.deepEqual(g, {
          type: 'FeatureCollection',
          features: [
@@ -56,7 +56,7 @@ describe('twkb', function () {
     })
 
     it('should decode multigeom with ids to geojson', function () {
-      var g = twkb.toGeoJSON(new Buffer('04070b0004020402000200020404', 'hex'))
+      var g = twkb.toGeoJSON(Buffer.from('04070b0004020402000200020404', 'hex'))
       assert.deepEqual(g, {
          type: 'FeatureCollection',
          features: [
@@ -81,7 +81,7 @@ describe('twkb', function () {
     })
 
     it('should decode collection to geojson', function () {
-      var g = twkb.toGeoJSON(new Buffer('070402000201000002020002080a0404', 'hex'))
+      var g = twkb.toGeoJSON(Buffer.from('070402000201000002020002080a0404', 'hex'))
       assert.deepEqual(g, {
          type: 'FeatureCollection',
          features: [
@@ -106,7 +106,7 @@ describe('twkb', function () {
     })
 
     it('should read multiple features', function () {
-      var g = twkb.toGeoJSON(new Buffer('0200020202080802000202020808', 'hex'))
+      var g = twkb.toGeoJSON(Buffer.from('0200020202080802000202020808', 'hex'))
       assert.equal(g.features.length, 2)
     })
   })
@@ -114,7 +114,7 @@ describe('twkb', function () {
   describe('read', function () {
     it('should decode linestring', function () {
       // select encode(ST_AsTWKB('LINESTRING(1 1,5 5)'::geometry), 'hex')
-      var f = twkb.read(new Buffer('02000202020808', 'hex'))[0]
+      var f = twkb.read(Buffer.from('02000202020808', 'hex'))[0]
       assert.equal(f.type, LINESTRING)
       assert(!f.ndims)
       assert(!f.bbox)
@@ -128,7 +128,7 @@ describe('twkb', function () {
 
     it('should decode linestring with bbox', function () {
       // select encode(ST_AsTWKB('LINESTRING(1 1,5 5)'::geometry, 0, 0, 0, true, true), 'hex')                                                                                                                                         ;
-      var f = twkb.read(new Buffer('020309020802080202020808', 'hex'))[0]
+      var f = twkb.read(Buffer.from('020309020802080202020808', 'hex'))[0]
       assert.equal(f.type, LINESTRING)
       assert(!f.ndims)
       assert.deepEqual(f.bbox, [1, 1, 5, 5])
@@ -141,7 +141,7 @@ describe('twkb', function () {
 
     it('should decode multilinestring with bbox', function () {
       // select encode(ST_AsTWKB('MULTILINESTRING((1 1,5 5), (1 2, 3 4))'::geometry, 0, 0, 0, true, true), 'hex')
-      var f = twkb.read(new Buffer('05030f020802080202020208080207050404', 'hex'))[0]
+      var f = twkb.read(Buffer.from('05030f020802080202020208080207050404', 'hex'))[0]
       assert.equal(f.type, MULTILINESTRING)
       assert(!f.ndims)
       assert.deepEqual(f.bbox, [1, 1, 5, 5])
@@ -158,7 +158,7 @@ describe('twkb', function () {
 
     it('should decode a point', function () {
       // select encode(ST_AsTWKB('POINT(1 2)'::geometry), 'hex')
-      var f = twkb.read(new Buffer('01000204', 'hex'))[0]
+      var f = twkb.read(Buffer.from('01000204', 'hex'))[0]
       assert.equal(f.type, POINT)
       assert(!f.ndims)
       assert.equal(f.coordinates[0], 1)
@@ -167,14 +167,14 @@ describe('twkb', function () {
 
     it('should decode a polygon with holes', function () {
       // select encode(ST_AsTWKB('POLYGON((0 0,2 0,2 2, 0 2, 0 0), (0 0, 0 1, 1 1, 1 0, 0 0))'::geometry, 0, 0, 0, true, true), 'hex')
-      var f = twkb.read(new Buffer('03031b000400040205000004000004030000030500000002020000010100', 'hex'))[0]
+      var f = twkb.read(Buffer.from('03031b000400040205000004000004030000030500000002020000010100', 'hex'))[0]
       assert.equal(f.type, POLYGON)
       assert(!f.ndims)
     })
 
     it('should decode a multigeom with ids', function () {
       // select st_astwkb(array_agg(geom::geometry), array_agg(id)) from (select 0 as id, 'POINT(0 1)' as geom union all select 1 as id, 'POINT(2 3)'as geom) a;
-      var f = twkb.read(new Buffer('04070b0004020402000200020404', 'hex'))[0]
+      var f = twkb.read(Buffer.from('04070b0004020402000200020404', 'hex'))[0]
       assert.equal(f.type, MULTIPOINT)
       assert(!f.ndims)
       assert.deepEqual(f.bbox, [0, 1, 2, 3])
@@ -191,7 +191,7 @@ describe('twkb', function () {
 
     it('should decode a collection', function () {
       // select st_astwkb(array_agg(geom::geometry), array_agg(id)) from (select 0 as id, 'POINT(0 1)' as geom union all select 1 as id, 'LINESTRING(4 5, 6 7)' as geom) a;
-      var f = twkb.read(new Buffer('070402000201000002020002080a0404', 'hex'))[0]
+      var f = twkb.read(Buffer.from('070402000201000002020002080a0404', 'hex'))[0]
       assert.equal(f.type, COLLECTION)
       assert.equal(f.geoms.length, 2)
       assert.equal(f.geoms[0].type, POINT)
