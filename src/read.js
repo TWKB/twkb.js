ecbd2b9e9ca2c6a3c79a1360e7041261567267c2
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2018-05-13 19:03:18 +0200

    Move to ECMAScript source and rollup as bundler (#12)

--- src/read.js
+++ src/read.js
@@ -1,4 +1,5 @@
-var readBuffer = require('./readBuffer')
+import readBuffer from './readBuffer'
+import { MAX } from './constants'
 
 /**
  * Read TWKB to object representation
@@ -6,25 +7,25 @@ var readBuffer = require('./readBuffer')
  * @param {number} offset Byte offset to start reading the binary buffer
  * @param {number} limit Stop translation after this many objects
  */
-function read (buffer, offset, limit) {
-  limit = limit || Number.MAX_VALUE
+export default function read (buffer, offset, limit) {
+  limit = limit || MAX
 
-  var ta_struct = {
+  const ta_struct = {
     buffer: buffer,
     cursor: offset === undefined ? 0 : offset,
     bufferLength: buffer.byteLength || buffer.length,
     refpoint: new Int32Array(4 /* max dims */)
   }
 
-  var data = []
-  var c = 0
+  const data = []
+  let c = 0
   while (ta_struct.cursor < ta_struct.bufferLength && c < limit) {
-    var res = readBuffer(ta_struct, limit)
+    const res = readBuffer(ta_struct, limit)
     if (res.length > 0) {
       // single geom type, add type info
       data.push({
         type: ta_struct.type,
-        offset: limit < Number.MAX_VALUE ? ta_struct.cursor : undefined,
+        offset: limit < MAX ? ta_struct.cursor : undefined,
         bbox: ta_struct.has_bbox ? ta_struct.bbox : undefined,
         coordinates: res
       })
@@ -38,5 +39,3 @@ function read (buffer, offset, limit) {
 
   return data
 }
-
-module.exports = read
