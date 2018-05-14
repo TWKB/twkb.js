31d4c569f4f6ad80506a155363b1caf31ef53f94
Björn Harrtell <bjorn@wololo.org>
2018-05-14 09:06:07 +0200

    Simplifications

--- src/toGeoJSON.js
+++ src/toGeoJSON.js
@@ -26,7 +26,7 @@ function createFeature (type, coordinates, id, ndims) {
   return {
     type: 'Feature',
     id: id,
-    geometry: getTransform(type)(coordinates, ndims)
+    geometry: getTransform(type)(type, coordinates, ndims)
   }
 }
 
@@ -38,7 +38,7 @@ function createFeaturesFromMulti (type, geoms, ids, ndims) {
 }
 
 // Create an array of GeoJSON feature objects
-function createFeaturesFromCollection (geoms, ids, ndims) {
+function createFeaturesFromCollection (type, geoms, ids, ndims) {
   return geoms.map((g, i) =>
     createFeature(g.type, g.coordinates, ids ? ids[i] : undefined, ndims)
   )
@@ -47,26 +47,25 @@ function createFeaturesFromCollection (geoms, ids, ndims) {
 // Map TWKB type to correct transformation function from intermediate representation to GeoJSON object
 function getTransform(type) {
   if (type === POINT)
-    return (coordinates, ndims) =>
+    return (type, coordinates, ndims) =>
       createGeometry(POINT, toCoords(coordinates, ndims)[0])
   else if (type === LINESTRING)
-    return (coordinates, ndims) =>
+    return (type, coordinates, ndims) =>
       createGeometry(LINESTRING, toCoords(coordinates, ndims))
   else if (type === POLYGON)
-    return (coordinates, ndims) =>
+    return (type, coordinates, ndims) =>
       createGeometry(POLYGON, coordinates.map(c => toCoords(c, ndims)))
   else if (type === MULTIPOINT)
-    return (geoms, ids, ndims) =>
+    return (type, geoms, ids, ndims) =>
       createFeaturesFromMulti(POINT, geoms, ids, ndims)
   else if (type === MULTILINESTRING)
-    return (geoms, ids, ndims) =>
+    return (type, geoms, ids, ndims) =>
       createFeaturesFromMulti(LINESTRING, geoms, ids, ndims)
   else if (type === MULTIPOLYGON)
-    return (geoms, ids, ndims) =>
+    return (type, geoms, ids, ndims) =>
       createFeaturesFromMulti(POLYGON, geoms, ids, ndims)
   else if (type === COLLECTION)
-    return (geoms, ids, ndims) =>
-      createFeaturesFromCollection(geoms, ids, ndims)
+    return createFeaturesFromCollection
   else
     unknownType(type)
 }
@@ -92,16 +91,16 @@ export default function toGeoJSON (buffer) {
     buffer: buffer,
     cursor: 0,
     bufferLength: buffer.byteLength || buffer.length,
-    refpoint: new Int32Array(4 /* max dims */)
+    refpoint: []
   }
 
   let features = []
   while (ta_struct.cursor < ta_struct.bufferLength) {
     const res = readBuffer(ta_struct, MAX)
     if (res.geoms)
-      features = features.concat(getTransform(res.type)(res.geoms, res.ids, ta_struct.ndims))
+      features = features.concat(getTransform(res.type)(res.type, res.geoms, res.ids, ta_struct.ndims))
     else
-      features.push({ type: 'Feature', geometry: getTransform(ta_struct.type)(res, ta_struct.ndims) })
+      features.push({ type: 'Feature', geometry: getTransform(ta_struct.type)(ta_struct.type, res, ta_struct.ndims) })
   }
 
   return {
